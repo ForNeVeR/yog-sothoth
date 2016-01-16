e856f00323256a7d596b59eb81157f1e83e044c4
Friedrich von Never <friedrich@fornever.me>
2016-01-16 18:27:01 +0600

    Initial architecture

---
+++ YogSothoth/Program.fs
@@ -0,0 +1,38 @@
+﻿module YogSothoth.Program
+
+open System.Configuration
+
+let private maxReconnectCount = 10
+
+let private logMessages jid password roomJid nickname =
+    let rec tryConnect tryNumber =
+        async {
+            try
+                return! Xmpp.connect jid password roomJid nickname
+            with
+            | e ->
+                printfn "Error: %A" e
+                do! Async.Sleep 5000
+
+                if tryNumber >= maxReconnectCount then
+                    return! tryConnect (tryNumber + 1)
+                else
+                    return! failwithf "Reconnect count exceeded"
+        }
+
+    async {
+        let! connection = tryConnect 0
+        Xmpp.logMessages connection
+        do! Xmpp.waitForTermination connection
+        return 0
+    }
+
+
+[<EntryPoint>]
+let main _ =
+    let jid = ConfigurationManager.AppSettings.["JID"]
+    let password = ConfigurationManager.AppSettings.["Password"]
+    let roomJid = ConfigurationManager.AppSettings.["RoomJID"]
+    let nickname = ConfigurationManager.AppSettings.["RoomNickname"]
+
+    Async.RunSynchronously (logMessages jid password roomJid nickname)
---
+++ YogSothoth/Xmpp.fs
@@ -0,0 +1,67 @@
+﻿module YogSothoth.Xmpp
+
+open System
+open System.Threading.Tasks
+open System.Xml
+
+open jabber
+open jabber.client
+open jabber.connection
+
+type XmppConnection =
+    { Client : JabberClient
+      RoomJid : JID }
+
+let private debug = false
+
+let private logInfo message =
+    printfn "%s" message
+
+let private logError = logInfo
+
+let private logElement logger header (element : XmlElement) =
+    logger (sprintf "%s: %s" header (element.ToString ()))
+
+let private logElementError = logElement logError
+
+let connect (jid : string) (password : string) (roomJid : string) (nickname : string) : Async<XmppConnection> =
+    let jid = JID jid
+    let roomJid = JID roomJid
+    let roomJid = JID (roomJid.User, roomJid.Server, nickname)
+    let client = new JabberClient (User = jid.User,
+                                   Server = jid.Server,
+                                   Password = password)
+
+    client.add_OnInvalidCertificate (fun _ _ _ _ -> true)
+
+    client.OnAuthError.Add (logElementError "Auth error")
+    client.OnError.Add (fun e -> logError (sprintf "Error: %s" (e.ToString ())))
+
+    if debug then
+        client.OnReadText.Add (fun text -> logInfo (sprintf "Read: %s" text))
+        client.OnWriteText.Add (fun text -> logInfo (sprintf "Write: %s" text))
+
+    client.OnConnect.Add (fun _ -> logInfo "Connected")
+
+    client.Connect ()
+
+    async {
+        let completionSource = TaskCompletionSource()
+        client.OnAuthenticate.Add (fun _ -> completionSource.SetResult ())
+
+        do! Async.AwaitTask completionSource.Task
+        return
+            { Client = client
+              RoomJid = roomJid }
+    }
+
+let logMessages { Client = client; RoomJid = roomJid } : Unit =
+    let roomManager = new ConferenceManager (Stream = client)
+    roomManager.add_OnJoin (fun room -> logInfo (sprintf "Joined room %A" room.JID))
+    let room = roomManager.GetRoom roomJid
+    room.Join ()
+
+let waitForTermination { Client = client } : Async<Unit> =
+    let completionSource = TaskCompletionSource()
+    client.OnDisconnect.Add (fun _ -> completionSource.SetResult ())
+    Async.AwaitTask completionSource.Task
