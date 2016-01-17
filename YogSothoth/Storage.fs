6a07c5ebadce5bd9b63e0a4c6d5e0113c901b0e9
Friedrich von Never <friedrich@fornever.me>
2016-01-17 13:05:07 +0600

    Storage: enable RavenDB

---
+++ YogSothoth/Storage.fs
@@ -0,0 +1,21 @@
+﻿module YogSothoth.Storage
+
+open System
+
+open Raven.Client
+open Raven.Client.Embedded
+
+type Message =
+    { Conference : string
+      Sender : string
+      DateTime : DateTime
+      Text : string }
+
+let initializeStore (dataDirectory : string) : IDocumentStore =
+    let store = new EmbeddableDocumentStore (DataDirectory = dataDirectory)
+    store.Initialize ()
+
+let save (store : IDocumentStore) (message : Message) : Unit =
+    use session = store.OpenSession ()
+    session.Store message
+    session.SaveChanges ()
