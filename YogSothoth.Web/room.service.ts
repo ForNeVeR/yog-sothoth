835800232a74dbf592dc02e8cc5400cf026a8cff
Friedrich von Never <friedrich@fornever.me>
2016-03-07 11:33:30 +0600

    RoomService: remove debug output

--- YogSothoth.Web/room.service.ts
+++ YogSothoth.Web/room.service.ts
@@ -6,9 +6,6 @@ export class RoomService {
     getRooms(): Promise<Room[]> {
         return fetch('api/rooms')
             .then(response => response.json())
-            .then(names => {
-                console.log('names', names);
-                return names.map(name => { return { name: name } });
-            });
+            .then(names => names.map(name => { return { name: name } }));
     }
 }
